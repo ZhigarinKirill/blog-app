e4162c948c6b32f97eb700d810657ece0d8f36ec
Zhigarin Kirill <zhigarink@gmail.com>
2022-11-03 19:04:26 +0300

    updated base structure and added home page

--- routes/post.py
+++ routes/post.py
@@ -3,3 +3,8 @@ from flask import Blueprint, request, redirect, url_for, render_template
 
 
 post_pages = Blueprint('posts', __name__)
+
+
+@post_pages.get("/")
+def display_post():
+    return render_template('home.html')
